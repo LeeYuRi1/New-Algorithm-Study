3c6b733e79a761d753c3331f8e87a16f58ad03dc
이유리 <dbf5156@naver.com>
2022-03-29 23:56:34 +0900

    Main_13458

---
+++ samsung/Main_13458.java
@@ -0,0 +1,23 @@
+package samsung;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_13458 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int B = Integer.parseInt(st2.nextToken());
+        int C = Integer.parseInt(st2.nextToken());
+        long ans = N;
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st1.nextToken()) - B;
+            if (num > 0) ans += num % C == 0 ? num / C : num / C + 1;
+        }
+        System.out.println(ans);
+    }
+}
