e7bd3fcfdcb0392de9cab01b394b1d503b386b71
이유리 <dbf5156@naver.com>
2022-04-01 16:24:56 +0900

    Main_13458_시험감독

--- samsung/Main_13458_시험감독.java
+++ samsung/Main_13458_시험감독.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_13458 {
+public class Main_13458_시험감독 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
