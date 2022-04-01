e5194a31c94706fb9c9d326ffe5ebdec3e6e39b7
이유리 <dbf5156@naver.com>
2022-04-01 16:14:47 +0900

    Main_1600_말이되고픈원숭이

--- baekjoon/Main_1600_말이되고픈원숭이.java
+++ baekjoon/Main_1600_말이되고픈원숭이.java
@@ -7,29 +7,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Point_1600 {
-    int x;
-    int y;
-    int k;
-    int count;
-
-    Point_1600(int x, int y, int k, int count) {
-        this.x = x;
-        this.y = y;
-        this.k = k;
-        this.count = count;
-    }
-}
-
 // 452ms, 87748KB
 public class Main_1600_말이되고픈원숭이 {
     private static int K, W, H, area[][];
     private static boolean[][][] visited;
-    private static Queue<Point_1600> queue = new LinkedList<>();
+    private static Queue<Point> queue = new LinkedList<>();
     private static int[] mx = {-1, 1, 0, 0};
     private static int[] my = {0, 0, -1, 1};
     private static int[] hx = {-1, -2, -2, -1, 1, 2, 2, 1};
     private static int[] hy = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+    private static class Point {
+        int x, y, k, count;
+
+        Point(int x, int y, int k, int count) {
+            this.x = x;
+            this.y = y;
+            this.k = k;
+            this.count = count;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -49,10 +46,10 @@ public class Main_1600_말이되고픈원숭이 {
 
     private static int bfs() {
         visited = new boolean[H][W][K + 1]; // 경로 겹침 -> 3차원 배열(x,y 위치에서 k회 남음)
-        queue.offer(new Point_1600(0, 0, K, 0));
+        queue.offer(new Point(0, 0, K, 0));
         visited[0][0][0] = true;
         while (!queue.isEmpty()) {
-            Point_1600 cur = queue.poll();
+            Point cur = queue.poll();
             if (cur.x == H - 1 && cur.y == W - 1) return cur.count;
             if (cur.k > 0) move(hx, hy, cur, cur.k - 1); // k가 0보다 크면 말 이동
             move(mx, my, cur, cur.k); // else 붙이기 말기! 원숭이 이동
@@ -60,13 +57,13 @@ public class Main_1600_말이되고픈원숭이 {
         return -1;
     }
 
-    private static void move(int[] dx, int[] dy, Point_1600 cur, int nk) {
+    private static void move(int[] dx, int[] dy, Point cur, int nk) {
         for (int i = 0; i < dx.length; i++) {
             int nx = cur.x + dx[i];
             int ny = cur.y + dy[i];
             if (nx < 0 || nx >= H || ny < 0 || ny >= W) continue;
             if (visited[nx][ny][nk] || area[nx][ny] == 1) continue;
-            queue.offer(new Point_1600(nx, ny, nk, cur.count + 1));
+            queue.offer(new Point(nx, ny, nk, cur.count + 1));
             visited[nx][ny][nk] = true;
         }
     }
