cc24ce3600c38dc618802118399a20b072cc42cf
이유리 <dbf5156@naver.com>
2022-04-12 19:07:50 +0900

    Main_1929_소수구하기

---
+++ baekjoon/Main_1929_소수구하기.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 368ms, 16144KB
+public class Main_1929_소수구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
